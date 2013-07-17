eaae33d22c28273f059b6303a3e31fe67781f331
Jim Rollenhagen <jim@jimrollenhagen.com>
2013-07-17 16:00:56 -0700

    add setup.py, setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import mailgun2
+
+setup(
+    name=mailgun2.__title__,
+    packages=[mailgun2.__title__],
+    version=mailgun2.__version__,
+    description='A python client for Mailgun API v2',
+    author=mailgun2.__author__,
+    author_email='tech@zerocater.com',
+    url='https://github.com/ZeroCater/python-mailgun2',
+    download_url='https://github.com/ZeroCater/python-mailgun2/archive/0.1.1.tar.gz',
+    keywords=['mailgun', 'email'],
+)
