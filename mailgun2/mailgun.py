b470c9171cc12964c96ae8e53fce32ea40e26dc9
Valentin-Costel Hăloiu <vially.ichb@gmail.com>
2014-03-02 22:58:53 +0200

    Add create and subscribe member methods

--- mailgun2/mailgun.py
+++ mailgun2/mailgun.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
+import json
 import requests
 
-
 BASE_URL = 'https://api.mailgun.net/v2'
+
+
 class Mailgun(object):
     def __init__(self, api_key, domain):
         self.auth = ('api', api_key)
@@ -27,3 +29,33 @@ class Mailgun(object):
         }
 
         return self.post('/messages', data)
+
+    def create_list(self, address, name=None, description=None, access_level=None):
+        data = {'address': address}
+        if name:
+            data['name'] = name
+
+        if description:
+            data['description'] = description
+
+        if access_level and access_level in ['readonly', 'members', 'everyone']:
+            data['access_level'] = access_level
+
+        return self.post('/lists', data)
+
+    def add_list_member(self, list_name, address, name=None, params=None,
+                        subscribed=True, upsert=False):
+        data = {'address': address}
+        if name:
+            data['name'] = name
+
+        if params:
+            data['vars'] = json.dumps(params) if isinstance(params, dict) else params
+
+        if not subscribed:
+            data['subscribed'] = 'no'
+
+        if upsert:
+            data['upsert'] = 'yes'
+
+        return self.post('/lists/%s/members' % list_name, data)
