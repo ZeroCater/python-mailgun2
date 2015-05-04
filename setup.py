fb580963522a736285ee92633b8c4b9bbd52156f
Albert Wang <albertyw@mit.edu>
2015-05-03 17:15:40 -0700

    Update setup.py to have version be based on variable

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author=mailgun2.__author__,
     author_email='tech@zerocater.com',
     url='https://github.com/ZeroCater/python-mailgun2',
-    download_url='https://github.com/ZeroCater/python-mailgun2/archive/0.1.3.tar.gz',
+    download_url="https://github.com/ZeroCater/python-mailgun2/archive/%s.tar.gz" % mailgun2.__version__,
     keywords=['mailgun', 'email'],
     install_requires=[
         'requests>=1.2.3',
